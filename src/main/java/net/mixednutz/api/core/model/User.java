e4c47a72def95db686709af438632ab8ad4af83d
apfesta <andy_festa@yahoo.com>
2018-11-01 16:00:01 -0400

    abstracted the userprofile stuff into its own interface

--- src/main/java/net/mixednutz/api/core/model/User.java
+++ src/main/java/net/mixednutz/api/core/model/User.java
@@ -1,22 +1,32 @@
 package net.mixednutz.api.core.model;
 
-import java.util.Map;
+import java.util.HashMap;
 
 import net.mixednutz.api.model.IUser;
+import net.mixednutz.api.model.IUserProfile;
 
 public class User extends UserSmall implements IUser {
 	
 	/**
 	 * Custom data provided by the network
 	 */
-	private Map<String, String> profileData;
+	private UserProfile profileData = new UserProfile();;
 
-	public Map<String, String> getProfileData() {
+	public UserProfile getProfileData() {
 		return profileData;
 	}
 
-	public void setProfileData(Map<String, String> data) {
+	public void setProfileData(UserProfile data) {
 		this.profileData = data;
+	}
+	
+	public static class UserProfile extends HashMap<String, String> implements IUserProfile {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -8461108474291713021L;
+		
 	}
 	
 }
